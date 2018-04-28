cdb72a161afd4e1b65d92626b1be6a23ff9c12dd
H Abdul <zungeru1@gmail.com>
2018-04-28 05:29:33 +0200

    changed component structure

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import Home from './components/Home.vue'
-import Favorites from './components/Favorites.vue'
-import Kast from './components/Kast.vue'
-import Search from './components/Search.vue'
-import Profile from './components/Profile.vue'
+import Home from './components/Home/Home.vue'
+import Favorites from './components/Favorites/Favorites.vue'
+import Kast from './components/Kast/Kast.vue'
+import Search from './components/Search/Search.vue'
+import Profile from './components/Profile/Profile.vue'
 
 export const routes = [
   {path: '', component: Home, name: 'home'},
