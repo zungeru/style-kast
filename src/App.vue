de33207e823a47e50c2ef9bee568275bd372961a
H Abdul <zungeru1@gmail.com>
2018-05-01 22:32:19 +0200

    Fixd Footer: Now using SVG

--- src/App.vue
+++ src/App.vue
@@ -50,12 +50,55 @@
       </main>
       <footer class="mdl-mini-footer">
         <div class="social-icons">
-          <router-link to="/" class="mobnav"><a><i class="material-icons mdl-color-text--black">home</i></a></router-link>
+          <router-link to="/" exact class="mobnav">
+            <a>
+              <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>
+                  <path d="M0 0h24v24H0z" fill="none"/>
+              </svg>
+            </a>
+          </router-link>
+          <router-link to="/favs" class="mobnav">
+            <a>
+              <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 0h24v24H0z" fill="none"/>
+                  <path d="M21.41 11.58l-9-9C12.05 2.22 11.55 2 11 2H4c-1.1 0-2 .9-2 2v7c0 .55.22 1.05.59 1.42l9 9c.36.36.86.58 1.41.58.55 0 1.05-.22 1.41-.59l7-7c.37-.36.59-.86.59-1.41 0-.55-.23-1.06-.59-1.42zM5.5 7C4.67 7 4 6.33 4 5.5S4.67 4 5.5 4 7 4.67 7 5.5 6.33 7 5.5 7zm11.77 8.27L13 19.54l-4.27-4.27C8.28 14.81 8 14.19 8 13.5c0-1.38 1.12-2.5 2.5-2.5.69 0 1.32.28 1.77.74l.73.72.73-.73c.45-.45 1.08-.73 1.77-.73 1.38 0 2.5 1.12 2.5 2.5 0 .69-.28 1.32-.73 1.77z"/>
+              </svg>
+            </a>
+          </router-link>
+          <router-link to="/kast" class="mobnav">
+            <a>
+              <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="9" cy="9" r="4"/>
+                <path d="M9 15c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4zm7.76-9.64l-1.68 1.69c.84 1.18.84 2.71 0 3.89l1.68 1.69c2.02-2.02 2.02-5.07 0-7.27zM20.07 2l-1.63 1.63c2.77 3.02 2.77 7.56 0 10.74L20.07 16c3.9-3.89 3.91-9.95 0-14z"/>
+                <path d="M0 0h24v24H0z" fill="none"/>
+              </svg>
+            </a>
+          </router-link>
+          <router-link to="/search" class="mobnav">
+            <a>
+              <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                  <path d="M0 0h24v24H0z" fill="none"/>
+              </svg>
+            </a>
+          </router-link>
+          <router-link to="/profile" class="mobnav">
+            <a>
+              <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
+                  <path d="M0 0h24v24H0z" fill="none"/>
+              </svg>
+            </a>
+          </router-link>
+        </div>
+        <!-- <div class="social-icons">
+          <router-link to="/" class="mobnav"><a><i class="material-icons">home</i></a></router-link>
           <router-link to="/favs" class="mobnav"><a><i class="material-icons mdl-color-text--black">loyalty</i></a></router-link>
           <router-link to="/kast" class="mobnav"><a><i class="material-icons mdl-color-text--black">record_voice_over</i></a></router-link>
           <router-link to="/search" class="mobnav"><a><i class="material-icons mdl-color-text--black">search</i></a></router-link>
           <router-link to="/profile" class="mobnav"><a><i class="material-icons mdl-color-text--black">person</i></a></router-link>
-        </div>
+        </div> -->
       </footer>
       <!-- End Main -->
   </div>
@@ -175,7 +218,6 @@ export default {
   max-width: 920px;
 } */
 .mdl-mini-footer{
-  background-color: gray;
   padding-left: 0px;
   padding-top: 10px;
   display: none;
@@ -190,30 +232,29 @@ export default {
 
 .mobnav {
       position: relative;  /* 1 */
-      width: 18.5%;
+      width: 17.5%;
       height: 35px;
       display: inline-block;
       text-align: center;
     }
 
-.mobnav i {
+.mobnav svg {
       margin: 0;
       position: absolute;  /* 2 */
       top: 50%;            /* 3 */
       transform: translate(0, -50%); /* 4 */
     }
 
-.mobnav a:hover i {
-      color: #f50057 !important;
-    }
-
-/* not working */
-.mobnav a:active i {
-      color: #f50057 !important;
+.mobnav a:hover svg {
+      fill: #f50057 !important;
     }
 
 .router-link-active {
   color: #f50057 !important;
+}
+
+.router-link-active svg {
+  fill: #F50057;
 }
 
 @media (max-width: 580px) {
