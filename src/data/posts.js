b1da0f617d3639fc173e7c47dccc5571a299a045
H Abdul <zungeru1@gmail.com>
2018-05-27 10:51:06 +0200

    lastest

--- src/data/posts.js
+++ src/data/posts.js
@@ -33,7 +33,7 @@
 export default [
   {
     id: 1,
-    name: 'Mido',
+    name: 'Mido1',
     age: 38,
     pics: [
       {src: 'static/img/1080/slide5_A.jpeg'},
@@ -49,7 +49,7 @@ export default [
   },
   {
     id: 2,
-    name: 'Q',
+    name: 'Q1',
     age: 30,
     pics: [
       {src: 'static/img/1080/slide1_A.jpeg'},
@@ -65,7 +65,535 @@ export default [
   },
   {
     id: 3,
-    name: 'RiRi',
+    name: 'RiRi1',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 4,
+    name: 'Mido2',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 5,
+    name: 'Q2',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 6,
+    name: 'RiRi2',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 9,
+    name: 'Mido3',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 8,
+    name: 'Q3',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 9,
+    name: 'RiRi3',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 10,
+    name: 'Mido4',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 11,
+    name: 'Q4',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 12,
+    name: 'RiRi4',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 13,
+    name: 'Mido5',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 14,
+    name: 'Q5',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 15,
+    name: 'RiRi5',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 16,
+    name: 'Mido6',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 17,
+    name: 'Q6',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 18,
+    name: 'RiRi6',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 19,
+    name: 'Mido7',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 20,
+    name: 'Q7',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 21,
+    name: 'RiRi7',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 22,
+    name: 'Mido8',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 23,
+    name: 'Q8',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 24,
+    name: 'RiRi8',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 25,
+    name: 'Mido9',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 26,
+    name: 'Q9',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 27,
+    name: 'RiRi9',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 28,
+    name: 'Mido10',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 29,
+    name: 'Q10',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 30,
+    name: 'RiRi10',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 31,
+    name: 'Mido11',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 32,
+    name: 'Q11',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 33,
+    name: 'RiRi11',
+    age: 28,
+    pics: [
+      {src: 'static/img/1080/slide4_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide5_A.jpeg'}
+    ],
+    piece: 'The Classic Party Dress',
+    price: '250.00',
+    place: 'Banana Republic',
+    note: 'This dress though!!!!, Everything!',
+    liked: false,
+    likedCount: 205
+  },
+  {
+    id: 34,
+    name: 'Mido12',
+    age: 38,
+    pics: [
+      {src: 'static/img/1080/slide5_A.jpeg'},
+      {src: 'static/img/1080/slide2_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'}
+    ],
+    piece: 'New Bomber Jacket',
+    price: '598.00',
+    place: 'Barneys New York',
+    note: 'OK, this jacket just made me smile. #smiles #mynewgoto',
+    liked: false,
+    likedCount: 125
+  },
+  {
+    id: 35,
+    name: 'Q12',
+    age: 30,
+    pics: [
+      {src: 'static/img/1080/slide1_A.jpeg'},
+      {src: 'static/img/1080/slide3_A.jpeg'},
+      {src: 'static/img/1080/slide4_A.jpeg'}
+    ],
+    piece: 'Brand New Jordans',
+    price: '320.00',
+    place: 'Uptown Sneaker Collective',
+    note: 'Stepping on in the freshest kicks, be the first to rock them #airj',
+    liked: false,
+    likedCount: 300
+  },
+  {
+    id: 36,
+    name: 'RiRi12',
     age: 28,
     pics: [
       {src: 'static/img/1080/slide4_A.jpeg'},
